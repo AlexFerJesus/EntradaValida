93cd0fbd857760e415f31467625fe54665d0ff40
AlexFerJesus <141464221+AlexFerJesus@users.noreply.github.com>
2023-12-01 23:01:22 +0000

    Pronto

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿﻿Console.WriteLine("+====Entrada Válida====+");
+
+int NumeroDigitado = -1;
+ 
+while (NumeroDigitado < 0 || NumeroDigitado > 9)
+{
+    Console.WriteLine("Digite um número de 1 a 9 ou 0 para encerrar a operação.");
+        NumeroDigitado = Convert.ToInt32(Console.ReadLine());
+}
+
+ if (NumeroDigitado == 0)
+ {
+    Console.WriteLine("Operação encerrada por: usuário");
+ }
+
+ else
+{
+    Console.WriteLine($"Número selecionado = {NumeroDigitado}");
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # EntradaValida
+    Programa que só permite a "entrada" do usuario com as informaçãoes condizentes.
